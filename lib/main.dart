5e30c557e147963cf34eb979ebb1c0750f2cd7a9
hectorlohaus <lohaus14@hotmail.com>
2026-02-25 11:30:10 -0300

    primero

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load environment variables
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL'] ?? '',
-    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    url: 'https://manvemwmogetigvawrmz.supabase.co',
+    anonKey: 'sb_publishable_58k64LmYqdzWGCRn8fOs5w_Py9K-Tru',
   );
 
   runApp(const ProviderScope(child: MainApp()));

7509c6e8e462b2778545439154e97a3719fee1f1
hectorlohaus <lohaus14@hotmail.com>
2026-04-08 18:21:57 -0400

    subida cambio pass

--- lib/widgets/responsive_scaffold.dart
+++ lib/widgets/responsive_scaffold.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' hide User;
 
 import 'package:gsn_control_de_proyectos/utils/app_colors.dart';
 import 'package:gsn_control_de_proyectos/providers/auth_provider.dart';
